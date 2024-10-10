2987fc1c650d689c682d2287ffedbf9bf6416697
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-10 11:22:12 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Link } from "./Link";
 export type { ILink } from "./Link";
 export type { ILinkType, ILinkSize } from "./Link/props";
+export { tokens as LinkTokens } from "./Link/Tokens/tokens";
